fa35997a7c9df90d66a9f659035407b385ef9657
Ava Dev <ava@example.com>
2026-09-16 15:00:00 +0000

    Barrels grow

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,2 +1,4 @@
 export * from "./session.js";
 export * from "./roles.js";
+export * from "./password.js";
+export * from "./webhooks.js";
--- packages/teams/src/index.ts
+++ packages/teams/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./invites.js";
 export * from "./policy.js";
+export * from "./members.js";
